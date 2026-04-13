2d0bb773644392b7fdf7b157d5a7faee194eb7a8
Joha1612 <152876163+Joha1612@users.noreply.github.com>
2026-04-13 09:06:32 +0600

    Add 'Cartoon' category to CATEGORY_URLS
    Added a new category 'Cartoon' to CATEGORY_URLS.

--- bot.py
+++ bot.py
@@ -36,6 +36,7 @@ CATEGORY_URLS = {
     "🌐 Hindi Dubbed": "https://fibwatch.art/videos/category/5",
     "🎭 Bangla Dubbed": "https://fibwatch.art/videos/category/852",
     "🎤 Natok": "https://fibwatch.art/videos/category/855",
+    "Cartoon":"https://fibwatch.art/videos/category/7",
 }
 
 
